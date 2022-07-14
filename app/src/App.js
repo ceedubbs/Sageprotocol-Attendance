155943e3e14013963ab434debe24316ae9becffd
Charlie <chbenj36@gmail.com>
2022-07-13 23:10:22 -0400

    didn't save last time. real first UI .js files commit

---
+++ app/src/App.js
@@ -0,0 +1,56 @@
+//This should probably be the App.js file
+
+import React from 'react'
+import { Main, Header, Button, SidePanel } from '@aragon/ui'
+
+import TokenInfoBox from './TokenInfoBox'
+import OwnershipDistributionBox from './OwnershipDistributionBox'
+
+function App() {
+    const [visible, setVisible] = useState(false)
+    const opener = React.createRef()
+
+    return (
+      <Main>
+        <Header
+        primary={
+          <>
+            Attendence
+            <Tag mode="identifier"> /*insert token abbreviation Capital Letters*/ </Tag>
+          </>
+        }
+        />
+        <Split
+        primary={<LoggerView tokenHolders={token.holders} />} //change this line up to use loggerView
+        secondary={
+          <>
+            <TokenInfoBox supply={token.supply} />
+            <OwnershipDistributionBox
+              supply={token.supply}
+              tokenHolders={token.holders}
+            />
+          </>
+        }
+      />
+      <Popover
+        title="Submitted!"
+        visible={visible}
+        opener={opener.current}
+        onClose={() => setVisible(false)}
+      >
+        Popover
+      </Popover>
+      </Main>
+    )
+  }
+
+/* We want 1 unique page here:
+
+1. An expandable list (this will technically be an array) which you can input wallet addresses
+2. A box to input the proposed amount of tokens to give
+3. Submit button that links to backend to create proposal to give those wallet addresses a certain amount of tokens each
+4. OnClick we want a success message of sorts (sidepanel perhaps)
+
+Note: on the side we want to take the two boxes from the tokens example
+
+*/
---
+++ app/src/Logger.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import {
+  DataView,
+  IdentityBadge,
+  ContextMenu,
+  ContextMenuItem,
+  IconPlus,
+  TextInput,
+  IconCheck,
+} from '@aragon/ui'
+
+const AttendenceList = []
+2
+function LoggerView({ AttendenceList }) {
+    const [addressValue, setAddressValue] = useState('')
+    const [nameValue, setNameValue] = useState('')
+    const [numValue, setNumValue] = useState(0)
+    return (
+        //need to add split here (primary and secondary)
+        <Split
+            primary={
+                <><TextInput
+                    title={'Address'}
+                    value={addressValue}
+                    onChange={event => {
+                        setAddressValue(event.target.value)
+                    } } /><TextInput
+                        title={'Name'}
+                        value={nameValue}
+                        onChange={event => {
+                            setNameValue(event.target.value)
+                        } } /><TextInput.Number
+                        title={'Number of Tokens'}
+                        value={numValue}
+                        onChange={event => {
+                            setNumValue(event.target.value)
+                        } } /><Button
+                        mode="strong"
+                        label="Add"
+                        icon={<IconPlus />}
+                        onClick={addToList()} /><DataView
+                        display="table"
+                        fields={['Holder', 'Name']}
+                        items={AttendenceList}
+                        renderEntry={entryParts}
+                        renderEntryActions={entryActions} /></>
+            }
+    //Text field with instructions - "Type addresses into the text field and press 'add'"
+    //text field that says "no one added yet" when array of addresses from dataview is empty
+    //TextInput Field with button that adds to dataview that lists addressfields of those already included in attendence count
+    //also add names of people
+
+    //use popover showing success message upon submission:
+            secondary={
+                <Button
+                mode="strong"
+                label="Submit"
+                icon={<IconCheck />}
+                onClick={bigSubmit()}
+                />
+            }
+        />
+    )
+}
+
+function addToList () {
+    AttendenceList.unshift([addressValue, nameValue]) //admitedly this might not be the proper way 
+    //to get the values of the address and name value fields
+    setAddressValue = ""
+    setNameValue = ""
+}
+
+function bigSubmit() {
+    //pass AttendenceList and inputNum to backend
+    AttendenceList = []
+    setNumValue = ""
+    setAddressValue = ""
+    setNameValue = ""
+}
+
+function entryParts([account, name]) {
+    return [<LocalIdentityBadge entity={account} />, name]
+  }
+
+export default LoggerView
