46722cc5a595dd96327011d9f55aed66e2e08e57
ceedubbs <swong24@andover.edu>
2022-12-12 11:19:25 -0500

    changed contract to dummy contract and edited arapp

---
+++ contracts/Attendance.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+import "@aragon/os/contracts/apps/AragonApp.sol";
+
+contract Attendance is AragonApp {
+
+    // ACL
+    bytes32 constant public DUMMY_ROLE = keccak256("DUMMY_ROLE");
+
+    function initialize() public onlyInit {
+        initialized();
+    }
+}
